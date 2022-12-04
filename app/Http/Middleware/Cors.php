7f14befe889ebb28d97eaf88fb06db614a7f21bf
Denis Mwangi <denniswanjohi.m@gmail.com>
2022-12-04 09:55:48 +0300

    digital ocean testing_2

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
+        header('Access-Control-Allow-Origin:  https://app.kazibin.adilirealestate.com');
+        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Methods:  POST, PUT');
         return $next($request);
     }
 }
