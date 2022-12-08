2dd1d6353fc96bf9fba9d3c605435764e0f1a00d
Denis Mwangi <denniswanjohi.m@gmail.com>
2022-12-08 12:20:17 +0300

    websockets correction_2

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,9 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
                 'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'port' => 6003,
+                'scheme' => 'https',
+                'useTLS' => true
             ],
         ],
 
