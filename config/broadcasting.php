ac0bbbae8e40835511ef0ca7e727805b7a3e8673
Denis Mwangi <denniswanjohi.m@gmail.com>
2022-12-10 12:11:10 +0300

    websockets correction_6

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,13 +40,8 @@ return [
                 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6003,
-                'scheme' => 'https',
-                'useTLS' => true,
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
-                'verify_peer'=>false
+                'scheme' => 'http',
+                'useTLS' => false
             ],
         ],
 
