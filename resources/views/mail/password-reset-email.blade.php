adc664db785794565123dbcbb0812e80d7be054f
Denis Mwangi <denniswanjohi.m@gmail.com>
2022-12-17 12:23:47 +0300

    mail sent to actual users

--- resources/views/mail/password-reset-email.blade.php
+++ resources/views/mail/password-reset-email.blade.php
@@ -35,7 +35,6 @@
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     
                     <img src="https://drive.google.com/file/d/1z7-lKxavVy96oBu1BuR7qTobvh0ljsvf/view?usp=share_link" alt="Image" class="icon"/>
-                    <img src="{{ $message->embed('assets/icon.png')) }}" alt="logo" title="Logo" style="display:block"  width="60" height="60"/>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
