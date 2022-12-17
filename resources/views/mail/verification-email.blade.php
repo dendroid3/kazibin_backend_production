bdd459865a965f5fea93df8fefcd89714c5901a0
Denis Mwangi <denniswanjohi.m@gmail.com>
2022-12-17 09:57:05 +0300

    mail sent to actual users

--- resources/views/mail/verification-email.blade.php
+++ resources/views/mail/verification-email.blade.php
@@ -29,7 +29,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen  sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen  sm:items-center py-4 sm:pt-0" style="color: white;">
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
