4ca36c94644770821c3fbb7d5ff498c34b94fd7a
Denis Wanjohi <kazibin66@gmail.com>
2024-08-26 20:38:16 +0300

    update script

--- resources/js/node_scripts/startXFB.sh
+++ resources/js/node_scripts/startXFB.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Start Xvfb
-Xvfb :99 -screen 0 1280x1024x24 &
+Xvfb :99 -screen 0 1280x1024x24 -nolisten tcp -ac +extension GLX +render -noreset &
 export DISPLAY=:99
+
 
 # Run Puppeteer script
 node resources/js/node_scripts/sendMessage.js "$1" "$2"
