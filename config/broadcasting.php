211cfaa89ce13ee84dffa60853f1f0eb6d1aa848
Denis Wanjohi <kazibin66@gmail.com>
2024-09-06 20:08:40 +0300

    IDK

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,15 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http',
-
-                'useTLS' => false
-
-            ],
+                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                 'host' => '127.0.0.1',
+                 'port' => 6001,
+                 'scheme' => 'http',
+             ],
         ],
 
         'ably' => [
