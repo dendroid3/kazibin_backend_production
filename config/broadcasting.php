059a83a5fbdb0b758e0cf9c8a159a83a9f98f08f
Denis Wanjohi <kazibin66@gmail.com>
2024-09-07 12:32:10 +0300

    IDK

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -43,8 +43,8 @@ return [
                 'port' => 6001,
                 'scheme' => 'https',
                 'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => (int) env('WEBSOCKETS_CURLOPT_SSL_VERIFYHOST', 2), // also tried 0
-                    CURLOPT_SSL_VERIFYPEER => (int) env('WEBSOCKETS_CURLOPT_SSL_VERIFYPEER', 1), // also tried 0
+                    CURLOPT_SSL_VERIFYHOST => (int) env('WEBSOCKETS_CURLOPT_SSL_VERIFYHOST', 0), // also tried 0
+                    CURLOPT_SSL_VERIFYPEER => (int) env('WEBSOCKETS_CURLOPT_SSL_VERIFYPEER', 0), // also tried 0
                 ]
             ],
         ],
