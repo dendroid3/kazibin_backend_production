47c6800f4bc6fa0d58dda011905af39f29f7a82d
Denis Wanjohi <kazibin66@gmail.com>
2024-09-07 11:45:58 +0300

    Changing

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+             'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
                 'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'host' => env('LARAVEL_WEBSOCKETS_HOST', '127.0.0.1'),
+                'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
+                'scheme' => 'https', // Secure connection
             ],
         ],
         'ably' => [
